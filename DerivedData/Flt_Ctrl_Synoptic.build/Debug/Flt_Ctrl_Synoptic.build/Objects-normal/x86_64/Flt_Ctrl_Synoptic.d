6cd55ed42ad898c9b08c911ecfbcffa0d7c3e322
Jordan Palmerr <jordan@dmpnet.org>
2023-12-31 13:10:53 +0000

    test

---
+++ DerivedData/Flt_Ctrl_Synoptic.build/Debug/Flt_Ctrl_Synoptic.build/Objects-normal/x86_64/Flt_Ctrl_Synoptic.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/jordanpalmer/Applications/VS_Code_C++/X-Plane-VS/Flt_Ctrl_Synoptic/Source/Flt_Ctrl_Synoptic.cpp \
+  SDK/CHeaders/XPLM/XPLMPlugin.h SDK/CHeaders/XPLM/XPLMDefs.h \
+  SDK/CHeaders/XPLM/XPLMDisplay.h SDK/CHeaders/XPLM/XPLMGraphics.h \
+  SDK/CHeaders/XPLM/XPLMProcessing.h SDK/CHeaders/XPLM/XPLMDataAccess.h \
+  SDK/CHeaders/XPLM/XPLMMenus.h SDK/CHeaders/XPLM/XPLMUtilities.h \
+  SDK/CHeaders/Widgets/XPWidgets.h SDK/CHeaders/Widgets/XPWidgetDefs.h \
+  SDK/CHeaders/Widgets/XPStandardWidgets.h \
+  SDK/CHeaders/XPLM/XPLMScenery.h
